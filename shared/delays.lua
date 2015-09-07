3f2577602e49f4c66b13a9314fd50b74b4cf378b
ING1337 <rubberduckish@gmx.de>
2015-09-07 23:53:13 +0200

    Update delays.lua

--- shared/delays.lua
+++ shared/delays.lua
@@ -1,22 +1,22 @@
-
 class 'DelayedEvents'
 
-function DelayedEvents:__init()
+function DelayedEvents:__init(trigger)
 	self.events	= {}
 	self.timer	= Timer()
+	self.tick	= trigger or "PreTick"
 	Events:Subscribe("DelayedEvent", self, self.AddEvent)
 end
 
 -- ####################################################################################################################################
 
 function DelayedEvents:AddEvent(args)
-	args.time = args.delay + self.timer:GetMilliseconds()
+	args.exp = (args.delay or args.time) + self.timer:GetMilliseconds()
 	if (#self.events == 0) then
 		table.insert(self.events, args)
-		self.sub = Events:Subscribe("PreTick", self, self.Ticker)
+		self.sub = Events:Subscribe(self.tick, self, self.Ticker)
 	else
 		for i = #self.events, 0, -1 do		-- may faster that way
-			if (i == 0) or (self.events[i].time <= args.time) then
+			if (i == 0) or (self.events[i].exp <= args.exp) then
 				table.insert(self.events, i + 1, args)
 				break
 			end
@@ -27,8 +27,8 @@ end
 function DelayedEvents:Ticker(args)
 	time = self.timer:GetMilliseconds()
 	while 1 do
-		if self.events[1] and (self.events[1].time <= time) then
-			item = self.events[1]		-- needed to call the remove function first...
+		if self.events[1] and (self.events[1].exp <= time) then
+			item = self.events[1]			-- needed to call the remove function first...
 			table.remove(self.events, 1)	-- avoids crashing the script in case of marshalling error
 			if (#self.events == 0) then Events:Unsubscribe(self.sub) end
 			Events:Fire(item.event, item.args)
@@ -37,7 +37,3 @@ function DelayedEvents:Ticker(args)
 		end
 	end
 end
-
--- ####################################################################################################################################
-
-delayedEvents = DelayedEvents()
